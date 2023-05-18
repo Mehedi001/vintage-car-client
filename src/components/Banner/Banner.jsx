2d6ff5e038f74210f183a7f49ffe4102a384b741
Mehedi001 <mh.hridoy5070@gmail.com>
2023-05-18 18:43:56 +0600

    banner & navbar done

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className='banner min-h-[200px] lg:min-h-[600px] flex justify-center items-center'>
+            <h3 className='text-white font-bold text-3xl lg:text-8xl'>HI Dear Kid Lover!!</h3>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,12 @@
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const Gallery = () => {
+    return (
+        <div>
+            <h2>Welcome to Showcase of Car Toys</h2>
+        </div>
+    );
+};
+
+export default Gallery;
