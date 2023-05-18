37578b87e2c582562f30ef8cfc7a46c46f5cfe0a
Mehedi001 <mh.hridoy5070@gmail.com>
2023-05-19 01:37:11 +0600

    gallery started worked

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -26,24 +26,7 @@ const Gallery = () => {
     return (
         <div>
             <h2 className='text-5xl font-semibold text-center my-10'> Welcome to Showcase of Car Toys</h2>
-            <Carousel
-            responsive={responsive}>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/ZSbt9Jk/1.jpg" alt="" /></div>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/X5mhsP2/2.jpg" alt="" /></div>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/QFnkLdY/3.jpg" alt="" /></div>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/8YJzKgn/4.jpg" alt="" /></div>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/1mHwvH4/5.jpg" alt="" /></div>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/dgcvThw/6.jpg" alt="" /></div>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/Zfhbnmj/7.jpg" alt="" /></div>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/Bw0qP8q/8.jpg" alt="" /></div>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/9v29ndj/9.jpg" alt="" /></div>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/yPHn2Bc/10.jpg" alt="" /></div>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/nnqG3S3/11.jpg" alt="" /></div>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/JmdYzzZ/12.jpg" alt="" /></div>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/0GBdXGT/13.jpg" alt="" /></div>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/2d9WhGf/14.jpg" alt="" /></div>
-                <div><img className='w-3/4 rounded-lg ' src="https://i.ibb.co/zfxbXm5/15.jpg" alt="" /></div>     
-            </Carousel>;
+            
         </div>
     );
 };
